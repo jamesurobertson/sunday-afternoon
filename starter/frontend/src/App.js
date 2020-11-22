6b69aca995ae73acb862669d8d81fecc5fff7e7c
James Robertson <jamesurobertson@gmail.com>
2020-11-22 14:29:16 -0600

    setState with counter

--- starter/frontend/src/App.js
+++ starter/frontend/src/App.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
-// import LoginFormPage from "./components/LoginFormPage";
-import * as sessionActions from "./store/session";
-import Navigation from "./components/Navigation";
+import React from "react";
+// import { useDispatch } from "react-redux";
+// import { Route, Switch } from "react-router-dom";
+// import SignupFormPage from "./components/SignupFormPage";
+// // import LoginFormPage from "./components/LoginFormPage";
+// import * as sessionActions from "./store/session";
+// import Navigation from "./components/Navigation";
 import HooksDemo from "./components/Hooksdemo";
 
 function App() {
--- starter/frontend/src/components/Hooksdemo.js
+++ starter/frontend/src/components/Hooksdemo.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 const HooksDemo = () => {
-  return <div> Hooks!</div>;
+  // useState returns an array where
+  // 1st element is the slice of state
+  // 2nd element is the function to updat the state
+  // like class components this.state = {counter: 0, name='james'}
+  // to upadte it we had invoke  this.setState({counter: this.state.counter +1})
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <>
+      <h1> Hooks!</h1>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+    </>
+  );
 };
 
 export default HooksDemo;
